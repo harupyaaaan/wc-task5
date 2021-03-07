a2867e891452e51990b807ff46c295591f31bae8
harupyaaaan <harukasama1225@outlook.jp>
2021-03-07 09:22:42 +0000

    [Finish]課題５

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
 
-
-
   get 'home/about' => 'homes#about'
   resources :books
 
+    get '/search' => 'search#search'
 end
